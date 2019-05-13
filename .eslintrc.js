c1322a9c07c216b62cf814f3258779a25017dc2d
dafeizhu <493833487@qq.com>
2019-05-13 10:56:12 +0800

    project init

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    "indent": ["off", 2],
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    hello world
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
 
