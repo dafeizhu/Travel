871d34f467b93bc3b2584a906da0aa8da769dee2
dafeizhu <493833487@qq.com>
2019-05-16 10:49:32 +0800

    border-bottom modify

--- src/pages/home/components/Weekend.vue
+++ src/pages/home/components/Weekend.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="title">周末去哪儿</div>
     <ul>
-      <li class="item border-bottom" v-for="item of list" :key="item.id">
+      <li class="item" v-for="item of list" :key="item.id">
         <div class="item-img-wrapper">
           <img class="item-img" :src='item.imgUrl'>
         </div>
@@ -30,20 +30,22 @@ export default {
     line-height .8rem
     background #eee
     text-indent .2rem
-  .item-img-wrapper
-    overflow hidden
-    height 0
-    padding-bottom 37.09%
-    .item-img
-      width 100%
-  .item-info
-    padding .1rem
-    .item-title
-      line-height .54rem
-      font-size .32rem
-      ellipsis()
-    .item-desc
-      line-height .4rem
-      color #ccc
-      ellipsis()
+  .item
+    border-bottom .1rem solid #eaeaea
+    .item-img-wrapper
+      overflow hidden
+      height 0
+      padding-bottom 37.09%
+      .item-img
+        width 100%
+    .item-info
+      padding .1rem
+      .item-title
+        line-height .54rem
+        font-size .32rem
+        ellipsis()
+      .item-desc
+        line-height .4rem
+        color #ccc
+        ellipsis()
 </style>
